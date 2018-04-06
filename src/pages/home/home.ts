e1a53993665f9e81f03df836c0df0943359436ee
Ricardo Caro <luricarolo@ittepic.edu.mx>
2018-04-06 14:56:33 -0600

    example

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { OtherPage } from "../otherPage/otherPage";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public myString:string ="Example 1 - This is just a string";
+  public otherPage:any = OtherPage;
+  public myJSON:any = {text:''};
+
   constructor(public navCtrl: NavController) {
 
+  }
+  gotoOtherPage():void{
+    this.navCtrl.push(OtherPage,{text:"Ejemplo 3-objeto"})
   }
 
 }
